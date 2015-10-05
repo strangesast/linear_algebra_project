80691b6f662ec1a32fd6395b1a77a674a21a91b0
Sam Zagrobelny <strangesast@gmail.com>
2015-10-05 10:42:10 -0400

    much better organized

--- plotting.py
+++ plotting.py
@@ -1,9 +1,18 @@
+# MA339 Project: Computer Graphics
+# 10/4/2015
+# Sam Zagrobelny, 0374837
+
 import matplotlib
 matplotlib.use('Agg')
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
-# original v
+########################################
+# Part I: Transform & Plot
+########################################
+
+# original v's
 v1 = [0, 0]
 v2 = [1, 0]
 v3 = [1, 1]
@@ -13,14 +22,17 @@ v4 = [0, 1]
 V = np.array([v1, v2, v3, v4]).T
 
 # figure setup
-figure_one = plt.figure(0)
-axis_one = plt.subplot(111, aspect=1.0)
-figure_two = plt.figure(1)
-axis_two = plt.subplot(111, aspect=1.0)
 
-########################################
-# Transform & Plot
-########################################
+figures = []
+axis = []
+fig_count = 5
+for i in range(1,fig_count+1):
+    fig = plt.figure(i)
+    figures.append(fig)
+    ax = fig.add_subplot(111, aspect=1.0)
+    axis.append(ax)
+
+# transform + plot action
 def plot_it(A, V, axis, label):
     if A is None: A = np.diag([1, 1])
     V_trans = np.dot(A, V)
@@ -31,68 +43,72 @@ def plot_it(A, V, axis, label):
 
     return line3d
 
-########################################
 # Original
-########################################
-original_lines, = plot_it(None, V, axis_one, 'original')
+for ax in axis:
+    original_lines, = plot_it(None, V, ax, 'original')
 
-########################################
 # Scale
-########################################
 A_scaled = np.diag([2, 2])  # transformation matrix
-scaled_lines, = plot_it(A_scaled, V, axis_one, 'scaled')
+scaled_lines, = plot_it(A_scaled, V, axis[0], 'scaled')
 
-########################################
 # Rotate
-########################################
 rot = np.radians(15) # convert to radians
 A_rot = np.array([
         [np.cos(rot), -np.sin(rot)],
         [np.sin(rot),  np.cos(rot)]
         ])
-rotated_lines, = plot_it(A_rot, V, axis_one, 'rotated')
+rotated_lines, = plot_it(A_rot, V, axis[1], 'rotated')
 
-########################################
 # Shear
-########################################
 k = -0.5 # skew factor 'k'
 A_shear = np.array([
     [1, k],
     [0, 1]
     ])
-sheared_lines, = plot_it(A_shear, V, axis_one, 'sheared')
+sheared_lines, = plot_it(A_shear, V, axis[2], 'sheared')
 
-########################################
 # Shear & Rotate
+A_shearrot = np.dot(A_rot, A_shear)
+shearrot_lines, = plot_it(A_shearrot, V, axis[3], 'sheared then rotated')
+
+# Rotate & Shear
+A_rotshear = np.dot(A_shear, A_rot)
+rotshear_lines, = plot_it(A_rotshear, V, axis[4], 'rotated then sheared')
+
+
+for ax in axis:
+    ylim = ax.get_ylim()
+    xlim = ax.get_xlim()
+
+    dy = abs(ylim[0] - ylim[1])
+    dx = abs(xlim[0] - xlim[1])
+
+    ax.axis([
+        xlim[0] - dx*0.1,
+        xlim[1] + dx*0.1,
+        ylim[0] - dy*0.1,
+        ylim[1] + dy*0.1
+        ])
+
+    ax.legend(
+        loc='upper center',
+        bbox_to_anchor=(0.5, -0.1),
+        fancybox = True,
+        ncol = 3
+    )
+
+    ax.set_title('Variety of Transformations')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+
+
+
+path = '.'
+for i, fig in enumerate(figures):
+    fig.savefig(os.path.join(path, "out{}.png".format(i)) , bbox_inches='tight')
+
 ########################################
-A_shearrot = np.dot(A_shear, A_rot)
-shearrot_lines, = plot_it(A_shearrot, V, axis_one, 'skewed then rotated')
+# Part II: Animation
+########################################
 
 
-ylim = axis_one.get_ylim()
-xlim = axis_one.get_xlim()
-
-dy = abs(ylim[0] - ylim[1])
-dx = abs(xlim[0] - xlim[1])
-
-axis_one.axis([
-    xlim[0] - dx*0.1,
-    xlim[1] + dx*0.1,
-    ylim[0] - dy*0.1,
-    ylim[1] + dy*0.1
-    ])
-
-axis_one.legend(
-    loc='upper center',
-    bbox_to_anchor=(0.5, -0.1),
-    fancybox = True,
-    ncol = 3
-)
-
-plt.title('Variety of Transformations')
-plt.xlabel('x')
-plt.ylabel('y')
-
-filepath = '/home/samuel/Downloads/out.png'
-figure_one.savefig(filepath.split('.')[0] + "1.png" , bbox_inches='tight')
-figure_two.savefig(filepath, bbox_inches='tight')
